4aa90921e0a364fc0f972a80aaf60305f9981117
Hirotaka Hachiya <hhachiya@sys.wakayama-u.ac.jp>
2019-07-22 13:56:54 +0900

    update

--- runExperiment.sh
+++ runExperiment.sh
@@ -6,7 +6,7 @@ for zDim in 8; do
 			for stopTrainThre in 0.05; do
 				for noiseSigma in 0.155; do
 					#python adversarialClassifier.py 0 $char $tri $noiseSigma $zDim 10000 $stopTrainThre 1 
-					python adversarialClassifier.py 2 $char $tri $noiseSigma $zDim 4000 $stopTrainThre 1.0 0
+					python adversarialClassifier.py 2 $char $tri $noiseSigma $zDim 4000 $stopTrainThre 3.0 0
 				done
 
 			done
